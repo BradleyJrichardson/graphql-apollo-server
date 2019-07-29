91978372fd58e18681fef060a0b41d83de3b0acb
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-29 18:51:22 +1000

    Authentication Middleware & Create/Delete Posts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,12 @@
-const postsResolvers = require('./posts');
-const usersResolvers = require('./users');
+const postsResolvers = require("./posts");
+const usersResolvers = require("./users");
 
 module.exports = {
   Query: {
     ...postsResolvers.Query
   },
   Mutation: {
-    ...usersResolvers.Mutation
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation
   }
 };
