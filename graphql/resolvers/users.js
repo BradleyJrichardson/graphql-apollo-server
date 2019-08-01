e4d654e46918449e15ea04291f702fb19495c73c
BradleyJrichardson <brdl.richardson@gmail.com>
2019-08-01 23:22:53 +1000

    .

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
-d;
+
 const {
   validateRegisterInput,
   validateLoginInput
