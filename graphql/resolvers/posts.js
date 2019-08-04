bb2ff30a7c3758941309993ce318ec710692f045
BradleyJrichardson <brdl.richardson@gmail.com>
2019-08-04 14:09:31 +1000

    server finished

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,7 +1,7 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
-const Post = require("../../models/Post");
-const checkAuth = require("../../utils/check-auth");
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Query: {
@@ -13,14 +13,13 @@ module.exports = {
         throw new Error(err);
       }
     },
-
     async getPost(_, { postId }) {
       try {
         const post = await Post.findById(postId);
         if (post) {
           return post;
         } else {
-          throw new Error("Post not found");
+          throw new Error('Post not found');
         }
       } catch (err) {
         throw new Error(err);
@@ -30,7 +29,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+
+      if (args.body.trim() === '') {
+        throw new Error('Post body must not be empty');
+      }
 
       const newPost = new Post({
         body,
@@ -40,9 +42,13 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      });
+
       return post;
     },
-
     async deletePost(_, { postId }, context) {
       const user = checkAuth(context);
 
@@ -50,13 +56,38 @@ module.exports = {
         const post = await Post.findById(postId);
         if (user.username === post.username) {
           await post.delete();
-          return "Post deleted successfully";
+          return 'Post deleted successfully';
         } else {
-          throw new AuthenticationError("Action not allowed");
+          throw new AuthenticationError('Action not allowed');
         }
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
